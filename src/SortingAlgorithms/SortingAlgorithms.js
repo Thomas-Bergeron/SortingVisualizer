d2de773572ff4ada83b963cac6e1b05e8e39569d
thomas-bergeron <bergeront@hotmail.fr>
2020-07-17 21:32:40 -0400

    Added somme comments

--- src/SortingAlgorithms/SortingAlgorithms.js
+++ src/SortingAlgorithms/SortingAlgorithms.js
@@ -1,39 +1,53 @@
+// calls the mergeSort and returns the animations array
 export function getMergeSortEvents(array) {
 	const animations = [];
 	mergeSort(array.slice(), 0, array.length - 1, animations);
 	return [animations];
 };
 
-
+// mergeSort function
 function mergeSort(array, startIndex, endIndex, animations) {
+	// only one element so no need to sort anymore
 	if (startIndex === endIndex) {
 		return array;
 	} else {
+		// finds the middleIndex of the array
 		const middleIndex = Math.floor((startIndex + endIndex) / 2);
+
+		//mergeSort the left and right parts of the array
 		mergeSort(array, startIndex, middleIndex, animations);
 		mergeSort(array, middleIndex + 1, endIndex, animations);
+
+		// return the left and right arrays merged together
 		return mergeArrays(array, startIndex, middleIndex, endIndex, animations);
 	}
 }
 
+// merges two arrays in sorted order
 function mergeArrays(array, startIndex, middleIndex, endIndex, animations) {
 	let mergedArray = [];
 	let i = startIndex;
 	let usedLeft = 0;
 	let j = middleIndex + 1;
+
+	// not at the end of the left or right part
 	while (i <= middleIndex && j <= endIndex) {
+		// compare the left and the right indexes value
 		animations.push([i + mergedArray.length - usedLeft, j ]);
 		animations.push([i + mergedArray.length - usedLeft, j ]);
 		if (array[i] <= array[j]) {
+			// put i value in the sorted array
 			animations.push([i + mergedArray.length - usedLeft++, mergedArray.length + startIndex, array[i]]);
 			mergedArray.push(array[i++]);
 		} else {
+			// put j value in the sorted array
 			animations.push([j, mergedArray.length + startIndex, array[j]]);
 			mergedArray.push(array[j++]);
 		}
 	}
 	
 	while (i <= middleIndex) {
+		// put every remaining values from the left side in the sorted array
 		animations.push([i + mergedArray.length - usedLeft, i + mergedArray.length - usedLeft]);
 		animations.push([i + mergedArray.length - usedLeft, i + mergedArray.length - usedLeft]);
 		animations.push([i + mergedArray.length - usedLeft++, mergedArray.length + startIndex, array[i]]);
@@ -41,12 +55,14 @@ function mergeArrays(array, startIndex, middleIndex, endIndex, animations) {
 	}
 	
 	while (j <= endIndex) {
+		// put every remaining values from the right side in the sorted array
 		animations.push([j, j]);
 		animations.push([j, j]);
 		animations.push([j, mergedArray.length + startIndex, array[j]]);
 		mergedArray.push(array[j++]);
 	}
 	
+	// place the sorted elements into the original array
 	for (let i = startIndex; i <= endIndex; i++) {
 		array[i] = mergedArray[i - startIndex];
 	}
@@ -54,77 +70,98 @@ function mergeArrays(array, startIndex, middleIndex, endIndex, animations) {
 	return array;
 }
 
+// calls the quickSort and returns the animations array
 export function getQuickSortEvents(array) {
 	const animations = [];
 	quickSort(array.slice(), 0, array.length - 1, animations);
 	return [animations];
 };
 
+// quickSort function
 function quickSort(array, startIndex, endIndex, animations) {
+	// only one element, already sorted
 	if (startIndex >= endIndex) return;
+
+	// partition the entire array
 	const p = partition(array, startIndex, endIndex, animations);
+
+	// quicksort left and right part of the partition middle element
 	quickSort(array, startIndex, p - 1, animations);
 	quickSort(array, p, endIndex, animations);
 }
 
+// selects the middle element as a pivot value and puts all higher values to the right and lower values to the left
 function partition(array, startIndex, endIndex, animations){
 	var i = startIndex;
 	var j = endIndex;
 
+	// sets entire array as yellow in animation
 	for (let k = startIndex; k <= endIndex; k++) {
 		animations.push([1, k]);
 	}
 	
+	// selects the middle element as pivot index
 	var pivotIndex = Math.floor((i + j) / 2);
+	// sets the index red in the animation
 	animations.push([2, pivotIndex])
+	// save the pivot value (important when the value changes place,
+	// you still need to compare to the same value, not the new one)
 	const pivot = array[pivotIndex];
 
-	animations.push([3, i]);
-	animations.push([3, j]);
-
+	
 	while (i <= j) {
+		// set comparing indexes to comparing color
+		animations.push([3, i]);
+		animations.push([3, j]);
+
+		// find the first element greater than pivot from the left side
 		while (array[i] < pivot) {
+			// set the index at normal color
 			animations.push([0, i]);
 			i++;
+			// set the comparing color to the new index
 			animations.push([3, i])
 		}
 		
+		// find the first element lower than pivot from the right side
 		while (array[j] > pivot) {
+			// set the index at normal color
 			animations.push([0, j]);
 			j--;
+			// set the comparing color to the new index
 			animations.push([3, j]);
 		}
 		
+		// new elements, needs swap
 		if (i <= j){
+			// set indexes to regular color
 			animations.push([0, i]);
 			animations.push([0, j]);
 
+			// add animation to swap elements
 			animations.push([4, i, j]);
-			
+			// swap elements
 			swap(array, i, j);
+			// increment elements
 			i++;
 			j--;
-
-			if (i < array.length) {
-				animations.push([3, i]);
-			}
-
-			if (j > 0) {
-				animations.push([3, j]);
-			}
 		}
 	}
-
+	
 	if (i < array.length) {
+		// return i index to regular color
 		animations.push([0, i]);
 	}
-
+	
 	if (j > 0) {
+		// return j index to regular color
 		animations.push([0, j]);
 	}
 
+	// returns pivot index
 	return i;
 }
+
 
 export function getHeapSortEvents(array) {
 	const animations = [];
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import './SortingVisualizer.css';
 import * as SortingAlgorithms from '../SortingAlgorithms/SortingAlgorithms.js'
 
+
+// Constants related to array elements
 const NB_ELEMENTS_INIT = 50;
 const MIN_ELEMENTS = 5;
 const MAX_ELEMENTS = 100;
 const MIN_ELEMENT_VALUE = 5;
 const MAX_ELEMENT_VALUE = 900;
 
+// Constants related to sleep
 const DEFAULT_SLEEP_TIME = 10;
 const MIN_SLEEP_TIME = 0;
-const MAX_SLEEP_TIME = 1000;
+const MAX_SLEEP_TIME = 500;
 
+// Enum of all possible algorithms
 const algorithms = {
 	MERGE: 0,
 	QUICK: 1,
@@ -26,56 +30,88 @@ export class SortingVisualizer extends React.Component {
 		this.state = {
 			array: [],
 			sleep: DEFAULT_SLEEP_TIME,
-			animation: false,
+			isAnimated: false,
 			algorithm: algorithms.MERGE,
 		};
 	}
 
 	componentDidMount() {
+		// Generate first array
 		this.generateArray(NB_ELEMENTS_INIT);
 	}
 
+	// Generates an array of nbElements with values between MIN_ELEMENT_VALUE and MAX_ELEMENT_VALUE
 	generateArray(nbElements) {
 		const array = [];
+		// Loop through all elements
 		for (let i = 0; i < nbElements; i++) {
+			// Sets the element to a random value
 			array.push(randomIntFromInterval(MIN_ELEMENT_VALUE, MAX_ELEMENT_VALUE));
 		}
 		this.setState({array});
 	}
 	
+	// handles mergeSort animation
 	async mergeSort() {
+		// array containing other arrays that each represent an animation
 		const [animations] = SortingAlgorithms.getMergeSortEvents(this.state.array);
+		// to manipulate bars
 		const arrayBars = document.getElementsByClassName('array-bar');
 		
-		this.setState({animation: true});
+		// start the animation
+		this.setState({isAnimated: true});
+
+		// loop through all animations
 		for (let i = 0; i < animations.length; i++) {
+			// first two animations are color change and the third is a swap
 			const isColorChange = i % 3 !== 2;
+
 			if (isColorChange) {
+				// get the bar IDs from an array in animation
 				const [barOneId, barTwoId] = animations[i];
+				// first color change is red, second is lightblue
 				const color = i % 3 === 0 ? 'red': 'lightblue';
+				// change bar color
 				arrayBars[barOneId].style.backgroundColor = color;
 				arrayBars[barTwoId].style.backgroundColor = color;
+				// delay of the current sleep time
 				await delay(this.state.sleep);
 			} else {
+				// get the information from the animation array
 				const [removeIndex, addIndex, value] = animations[i];
+				
+				// change the element position
 				const tempArray = this.state.array.slice();
 				tempArray.splice(removeIndex, 1);
 				tempArray.splice(addIndex, 0, value);
 				this.setState({array: tempArray});
+
+				// delay of the current sleep time
 				await delay(this.state.sleep);
 			}
 		}
-		this.setState({animation: false});
+		// end the animation
+		this.setState({isAnimated: false});
 	}
 	
+	// handles quickSort animatoin
 	async quickSort() {
+		// array containing other arrays that each represent an animation
 		const [animations] = SortingAlgorithms.getQuickSortEvents(this.state.array);
+		// to manipulate bars
 		const arrayBars = document.getElementsByClassName('array-bar');
 		
-		this.setState({animation: true});
+		// start the animation
+		this.setState({isAnimated: true});
+
+		// loop through all the animations
 		for (let i = 0; i < animations.length; i++) {
+			// get information from the animation array
 			const [animation, barOneId, barTwoId] = animations[i];
+			
+			// animation is a swap
 			if (animation === 4) {
+				// swap the elements
 				let tempArray = this.state.array.slice();
 				let value1 = tempArray[barOneId];
 				let value2 = tempArray[barTwoId];
@@ -84,27 +120,44 @@ export class SortingVisualizer extends React.Component {
 				tempArray.splice(barTwoId, 1);
 				tempArray.splice(barTwoId, 0, value1);
 				this.setState({array: tempArray});
+				
+				// delay of the current sleep time
 				await delay(this.state.sleep);
 			} else {
+				// select color to use
 				let color = animation === 1 ? 'yellow' : animation === 2 ? 'red' : animation === 3 ? 'blue' : animation === 5 ? 'orange' : 'lightblue';
+				
+				// change bar color
 				arrayBars[barOneId].style.backgroundColor = color;
+				
+				// delay of the current sleep time if the color is blue
 				if (color === 'blue') {
 					await delay(this.state.sleep);
 				}
 			}
 		}
-		this.setState({animation: false});
+		// end of animation
+		this.setState({isAnimated: false});
 	}
 	
+	// handles heapSort animation
 	async heapSort() {
+		// array containing other arrays that each represent an animation
 		const [animations] = SortingAlgorithms.getHeapSortEvents(this.state.array);
+		// to manipulate bars
 		const arrayBars = document.getElementsByClassName('array-bar');
 
-		this.setState({animation: true});
-		console.log(animations);
+		// start of animation
+		this.setState({isAnimated: true});
+
+		// loop through all the animations
 		for (let i = 0; i < animations.length; i++) {
+			// get information from animation array
 			const [animation, barOneId, barTwoId] = animations[i];
+
+			// animation is a swap
 			if (animation === 2) {
+				// swap elements
 				let tempArray = this.state.array.slice();
 				let value1 = tempArray[barOneId];
 				let value2 = tempArray[barTwoId];
@@ -115,25 +168,39 @@ export class SortingVisualizer extends React.Component {
 				this.setState({array: tempArray});
 				await delay(this.state.sleep);
 			} else {
+				// select color to use
 				let color = animation === 1 ? 'red' : 'lightblue';
+
+				// change bar color
 				arrayBars[barOneId].style.backgroundColor = color;
+
+				// delay of the current sleep time if the color is red
 				if (color === 'red') {
 					await delay(this.state.sleep);
 				}
 			}
 		}
-		this.setState({animation: false});
+		// end of animation
+		this.setState({isAnimated: false});
 	}
 
 	async bubbleSort() {
+		// array containing other arrays that each represent an animation
 		const [animations] = SortingAlgorithms.getBubbleSortEvents(this.state.array);
+		// to manipulate bars
 		const arrayBars = document.getElementsByClassName('array-bar');
 
-		this.setState({animation: true});
-		console.log(animations);
+		// start of animation
+		this.setState({isAnimated: true});
+
+		// loop through all animations
 		for (let i = 0; i < animations.length; i++) {
+			// get information from animation array
 			const [animation, barOneId, barTwoId] = animations[i];
+
+			// animation is a swap
 			if (animation === 2) {
+				// swap the elements
 				let tempArray = this.state.array.slice();
 				let value1 = tempArray[barOneId];
 				let value2 = tempArray[barTwoId];
@@ -142,40 +209,48 @@ export class SortingVisualizer extends React.Component {
 				tempArray.splice(barTwoId, 1);
 				tempArray.splice(barTwoId, 0, value1);
 				this.setState({array: tempArray});
+
+				// delay of the current sleep time
 				await delay(this.state.sleep);
 			} else {
+				// select color to use
 				let color = animation === 1 ? 'red' : 'lightblue';
+
+				// change bar color
 				arrayBars[barOneId].style.backgroundColor = color;
 				arrayBars[barTwoId].style.backgroundColor = color;
+
+				// delay of current sleep time if color is red
 				if (color === 'red') {
 					await delay(this.state.sleep);
 				}
 			}
 		}
-		this.setState({animation: false});
+		// end of animation
+		this.setState({isAnimated: false});
 	}
 
+	// selects the current sorting algorithm and calls the correct method to animate
 	sort() {
-		if (!this.state.animation) {
-			switch (this.state.algorithm) {
-				case algorithms.MERGE:
-					this.mergeSort();
-					break;
-				case algorithms.QUICK:
-					this.quickSort();
-					break;
-				case algorithms.HEAP:
-					this.heapSort();
-					break;
-				case algorithms.BUBBLE:
-					this.bubbleSort();
-					break;
-				default:
-					break;
-			}
+		switch (this.state.algorithm) {
+			case algorithms.MERGE:
+				this.mergeSort();
+				break;
+			case algorithms.QUICK:
+				this.quickSort();
+				break;
+			case algorithms.HEAP:
+				this.heapSort();
+				break;
+			case algorithms.BUBBLE:
+				this.bubbleSort();
+				break;
+			default:
+				break;
 		}
 	}
 
+	// allows to change the selected algorithm and radio button
 	handleRadioEvent(event){
 		switch (event.target.id){
 			case "merge":
@@ -196,7 +271,7 @@ export class SortingVisualizer extends React.Component {
 	}
 
 	render() {
-		const {array, sleep, animation} = this.state;
+		const {array, sleep, isAnimated} = this.state;
 		
 		return (
 			<div className="whole-page">
@@ -213,7 +288,7 @@ export class SortingVisualizer extends React.Component {
 				</div>
 				
 				<div className="footer">
-					<button className="button text-button" onClick={() => this.generateArray(this.state.array.length)} disabled={animation}>Generate New Array</button>
+					<button className="button text-button" onClick={() => this.generateArray(this.state.array.length)} disabled={isAnimated}>Generate New Array</button>
 
 					<div className="option-container">
 						<div>
@@ -224,7 +299,7 @@ export class SortingVisualizer extends React.Component {
 									type="range"
 									id="sliderElements"
 									className="slider"
-									disabled={animation}
+									disabled={isAnimated}
 									min={MIN_ELEMENTS}
 									max={MAX_ELEMENTS}
 									defaultValue={NB_ELEMENTS_INIT}
@@ -235,7 +310,7 @@ export class SortingVisualizer extends React.Component {
 						<div>
 							<div className="slider-container">
 								<label>Sorting Speed</label>
-								<label>{sleep}</label>
+								<label>{parseFloat(((MAX_SLEEP_TIME - MIN_SLEEP_TIME - sleep) / (MAX_SLEEP_TIME - MIN_SLEEP_TIME) * 100).toFixed(1))} %</label>
 								<input
 									type="range"
 									id="sliderSleep"
@@ -252,30 +327,30 @@ export class SortingVisualizer extends React.Component {
 					<div className="algorithm-container">
 						<input type="radio" name="algorithms" id="merge" checked={this.state.algorithm === algorithms.MERGE}
 																		onChange={(event) => {this.handleRadioEvent(event)}}
-																		disabled={animation}></input>
+																		disabled={isAnimated}></input>
 							<label htmlFor="merge" className="algorithm-label algo1">
 								<span>Merge Sort</span>
 							</label>
 						<input type="radio" name="algorithms" id="quick" checked={this.state.algorithm === algorithms.QUICK}
 																		onChange={(event) => {this.handleRadioEvent(event)}}
-																		disabled={animation}></input>
+																		disabled={isAnimated}></input>
 							<label htmlFor="quick" className="algorithm-label algo2">
 								<span>Quick Sort</span>
 							</label>
 						<input type="radio" name="algorithms" id="heap" checked={this.state.algorithm === algorithms.HEAP}
 																		onChange={(event) => {this.handleRadioEvent(event)}}
-																		disabled={animation}></input>
+																		disabled={isAnimated}></input>
 							<label htmlFor="heap" className="algorithm-label algo3">
 								<span>Heap Sort</span>
 							</label>
 						<input type="radio" name="algorithms" id="bubble" checked={this.state.algorithm === algorithms.BUBBLE}
 																		onChange={(event) => {this.handleRadioEvent(event)}}
-																		disabled={animation}></input>
+																		disabled={isAnimated}></input>
 							<label htmlFor="bubble" className="algorithm-label algo4">
 								<span>Bubble Sort</span>
 							</label>
 					</div>
-					<button className="button round-button" onClick={() => this.sort()} disabled={animation}>Sort!</button>
+					<button className="button round-button" onClick={() => this.sort()} disabled={isAnimated}>Sort!</button>
 				</div>
 
 			</div>
@@ -284,10 +359,12 @@ export class SortingVisualizer extends React.Component {
 	}
 }
 
+// returns an int between the two provided values
 function randomIntFromInterval (min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// delay the number of ms provided and returns when it's done
 function delay(ms) {
 	return new Promise(res => setTimeout(res, ms));
 }
